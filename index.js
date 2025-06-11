6cae3e02039bcbe0bc15f4eef2d35bc860d1c6a5
Yo0l0 <126848206+Yo0l0@users.noreply.github.com>
2025-06-11 08:55:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ app.post('/clear_vote', (req, res) => {
   }
   res.status(404).send('User not found');
 });
-app.listen(3000, () => {
-  console.log('🚀 Listening on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Listening on port ${PORT}`);
 });
