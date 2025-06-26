f4980b07ab3e7fbfe644e3b12356fd4dd8c1fcb0
Yo0l0 <126848206+Yo0l0@users.noreply.github.com>
2025-06-26 14:14:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -292,6 +292,7 @@ app.get('/dashboard', async (req, res) => {
 
         let html = `
         <head>
+            <link rel="icon" type="image/png" href="https://raw.githubusercontent.com/Yo0l0/ssss/refs/heads/main/GengarImages.png">
             <style>
                 body { font-family: Arial, sans-serif; background: #0d001d; color: white; text-align: center; padding: 20px; }
                 .grid { display: grid; grid-template-columns: repeat(auto-fit, minmax(220px, 1fr)); gap: 20px; justify-items: center; margin-top: 30px; }
