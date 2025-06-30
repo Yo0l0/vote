18468faddb80a9aed7876e5cb3d349c4de5bcc01
Yo0l0 <126848206+Yo0l0@users.noreply.github.com>
2025-06-30 11:02:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.get('/stats', async (req, res) => {
         let totalUsers = 0;
         let droppedToday = 0;
 
-        const BOT_START_DATE = new Date('2024-06-01'); // Set your real start date
+        const BOT_START_DATE = new Date('2024-04-20'); // Set your real start date
         const now = new Date();
         const todayStart = new Date(now.getFullYear(), now.getMonth(), now.getDate()).getTime();
 
